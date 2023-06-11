7c3ac361b0ad1398686927a01dfccf7431db86ce
Praveen Prajapati <pk20210845@gmail.com>
2023-06-11 19:41:18 +0530

    adding user

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ exports.person = require("./lib/person.js");
 exports.book = require("./lib/book.js");
 exports.country = require("./lib/country.js");
 exports.animal = require("./lib/animal.js");
+exports.user = require("./lib/user.js");
+exports.company = require("./lib/company.js");
+exports.image = require("./lib/image.js");
