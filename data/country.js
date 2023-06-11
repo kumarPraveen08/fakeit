76128ef2fcfb8a0c44419f65238ff1ce34e8fccf
Praveen Prajapati <pk20210845@gmail.com>
2023-06-11 09:37:28 +0530

    added person, book and country methods

---
+++ data/country.js
@@ -0,0 +1,38 @@
+exports.Country = [
+  {
+    name: "India",
+    currency: {
+      short: "INR",
+      long: "Indian Rupee",
+    },
+    callCode: "+91",
+    isoCode: "IN",
+    timezone: "Asia/Kolkata",
+    languages: ["Hindi", "English"],
+    tld: [".in", ".co.in"],
+  },
+  {
+    name: "Japan",
+    currency: {
+      short: "JPY",
+      long: "Japanese Yen",
+    },
+    callCode: "+81",
+    isoCode: "JP",
+    timezone: "Asia/Tokyo",
+    languages: ["Japanese"],
+    tld: [".jp"],
+  },
+  {
+    name: "Korea",
+    currency: {
+      short: "KRW",
+      long: "Korend Won",
+    },
+    callCode: "+82",
+    isoCode: "KR",
+    timezone: "Asia/Seoul",
+    languages: ["Korean"],
+    tld: [".kr"],
+  },
+];
--- index.js
+++ index.js
@@ -1,32 +1,3 @@
-const book = require("./data/book.js");
-const random = require("./random.js");
-
-// BOOK
-
-/* @returns {string} 
-- A book title */
-exports.title = () => {
-  return book.Books.title[random.r(book.Books.title)];
-};
-
-/* @returns {string} 
-- A book author name */
-exports.author = () => {
-  return book.Books.author[random.r(book.Books.author)];
-};
-
-/* @returns {string} 
-- A book genre */
-exports.genre = () => {
-  return book.Books.genre[random.r(book.Books.genre)];
-};
-
-/* @returns {object} 
-- A book name */
-exports.bookObject = () => {
-  return {
-    title: this.title(),
-    author: this.author(),
-    genre: this.genre(),
-  };
-};
+exports.person = require("./lib/person.js");
+exports.book = require("./lib/book.js");
+exports.country = require("./lib/country.js");
